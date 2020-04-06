02c62e42cad476cb3120b38c1ac192eef9863a02
Kieran Mc Gowan <kieranmcgowan21@hotmail.com>
2020-04-06 00:41:45 +0000

    adding automation

--- hive/covid/run-me.sh
+++ hive/covid/run-me.sh
@@ -1,5 +1,14 @@
+DATE=`date -d '-1 day' +%F`
+#NOTE: Strip quotesfrom csv
+#wget https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.6.zip 
+#unzip simplemaps_worldcities_basicv1.6.zip 
+
 #hive -f create-db.ql
+wget https://opendata.ecdc.europa.eu/covid19/casedistribution/csv
+mv covid-19.csv covid-19-${DATE}.csv
+mv covid-latest-report.csv covid-latest-report-${DATE}.csv
+mv csv covid-19.csv 
 hive -f covid-19.ql
 hive -f worldcities.ql
 hive -f worldcities-summary.ql
-hive -f query.ql 
+hive -f query.ql > covid-latest-report.csv
